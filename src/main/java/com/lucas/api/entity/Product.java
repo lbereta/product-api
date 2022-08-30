76b672f5b74836ed64d67962c61232ca0942d3d5
Lucas Bereta <lucas.bereta@hotmail.com>
2022-08-30 11:48:02 -0300

    :construction: Work entity class

---
+++ src/main/java/com/lucas/api/entity/Product.java
@@ -0,0 +1,26 @@
+package com.lucas.api.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+
+public class Product implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String description;
+    private double price;
+}
